b124907402155384d1b88418222f421726637168
liblaf <30631553+liblaf@users.noreply.github.com>
2024-10-16 16:25:58 +0800

    fix: correct configuration paths and update workflow scripts
    - Adjusted paths in configuration files to ensure correct inheritance and functionality.
    - Updated workflow scripts to include missing parameters and improve execution logic.
    - Removed redundant cleanup steps in the MegaLinter workflow to streamline the process.

--- copier-update/main.sh
+++ copier-update/main.sh
@@ -4,8 +4,10 @@ set -o nounset
 set -o pipefail
 
 copier=(pipx run --pip-args cookiecutter copier)
+if [[ ! -d .github/copier/ ]]; then
+  exit
+fi
 readarray -t answers_files < <(find .github/copier/ -iname ".copier-answers.*")
-
 for answers_file in "${answers_files[@]}"; do
   "${copier[@]}" update --trust --answers-file "$answers_file" --defaults
 done
