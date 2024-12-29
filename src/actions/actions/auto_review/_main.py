b744ee133f16a0beb07309fe28c2f446c08a5fc6
liblaf <30631553+liblaf@users.noreply.github.com>
2024-12-29 17:24:53 +0800

    refactor: remove user-specific details from auto-review bot
    Simplifies the auto-review bot by removing unnecessary user authentication
    and email generation. This reduces complexity and focuses the bot solely on
    approving pull requests.

--- src/actions/actions/auto_review/_main.py
+++ src/actions/actions/auto_review/_main.py
@@ -1,7 +1,6 @@
 from typing import Any
 
 import githubkit
-import githubkit.versions.latest.models as m
 
 import actions
 
@@ -12,10 +11,6 @@ from ._inputs import Inputs
 async def main(inputs: Inputs) -> None:
     _: Any
     gh = githubkit.GitHub(inputs.token)
-    user: m.PrivateUser | m.PublicUser = (
-        await gh.rest.users.async_get_authenticated()
-    ).parsed_data
-    email: str = f"{user.id}+{user.login}@users.noreply.github.com"
     owner: str
     repo: str
     owner, _, repo = inputs.repo.partition("/")
@@ -34,9 +29,5 @@ async def main(inputs: Inputs) -> None:
                 break
         else:
             await gh.rest.pulls.async_create_review(
-                owner=owner,
-                repo=repo,
-                pull_number=pr.number,
-                body=f"Bot-Review: {user.login} <{email}>",
-                event="APPROVE",
+                owner=owner, repo=repo, pull_number=pr.number, event="APPROVE"
             )
