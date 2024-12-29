d685282ee0571b5b1b2ed4e4b6228b5c1956d1b9
liblaf <30631553+liblaf@users.noreply.github.com>
2024-12-29 16:20:36 +0800

    ci: enhance copier update script with color and grouping
    - Adds `FORCE_COLOR=1` to enable colored output in the copier update process
    - Wraps each copier update operation in GitHub Actions group for better log organization

--- copier-update/main.sh
+++ copier-update/main.sh
@@ -3,12 +3,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+export FORCE_COLOR=1
+
 copier=(pipx run --pip-args cookiecutter copier)
 if [[ ! -d .github/copier/ ]]; then
   exit
 fi
 readarray -t answers_files < <(find .github/copier/ -iname ".copier-answers*")
 for answers_file in "${answers_files[@]}"; do
-  echo "update $answers_file"
+  echo "::group::$answers_file"
   "${copier[@]}" update --skip-answered --trust --answers-file "$answers_file"
+  echo "::endgroup::"
 done
