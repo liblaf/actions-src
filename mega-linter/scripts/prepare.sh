9b67026d734213992d2d0a88cf93c1b204f9894e
liblaf <30631553+liblaf@users.noreply.github.com>
2025-01-25 23:21:21 +0800

    refactor: streamline MegaLinter configuration handling
    - Moves MegaLinter config discovery logic from action.yaml to prepare.sh
    - Simplifies action.yaml by removing redundant steps
    - Ensures consistent config file cleanup in remove.sh

--- mega-linter/scripts/prepare.sh
+++ mega-linter/scripts/prepare.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-set -o errexit -o nounset -o pipefail
+set -o errexit
+set -o nounset
+set -o pipefail
 
 LINTER_RULES_PATH="https://github.com/liblaf/.github/raw/refs/heads/main/.github/linters"
 LINTER_RULES=(
@@ -10,7 +12,6 @@ LINTER_RULES=(
   pyrightconfig.json
 )
 MISSING_LINTER_RULES=()
-
 function download() {
   local_path=".github/linters/$1"
   remote_path="$LINTER_RULES_PATH/$1"
@@ -21,9 +22,25 @@ function download() {
   mkdir --parents --verbose ".github/linters"
   wget --output-document="$local_path" "$remote_path"
 }
-
 for rule in "${LINTER_RULES[@]}"; do
   download "$rule"
 done
-
 echo "MISSING_LINTER_RULES=${MISSING_LINTER_RULES[*]}" >> "$GITHUB_ENV"
+
+if [[ -f ".mega-linter.yml" ]]; then
+  echo "MISSING_MEGALINTER_CONFIG=false" >> "$GITHUB_ENV"
+else
+  MEGALINTER_CONFIG_OPTIONS=(
+    .mega-linter.yaml
+    .github/.mega-linter.yml
+    .github/.mega-linter.yaml
+    "$GITHUB_ACTION_PATH/.mega-linter.yaml"
+  )
+  for config in "${MEGALINTER_CONFIG_OPTIONS[@]}"; do
+    if [[ -f $config ]]; then
+      cp "$config" ".mega-linter.yml"
+      echo "MISSING_MEGALINTER_CONFIG=true" >> "$GITHUB_ENV"
+      break
+    fi
+  done
+fi
--- mega-linter/scripts/remove.sh
+++ mega-linter/scripts/remove.sh
@@ -7,3 +7,7 @@ IFS=" " read -a MISSING_LINTER_RULES -r <<< "$MISSING_LINTER_RULES"
 for rule in "${MISSING_LINTER_RULES[@]}"; do
   rm --force --verbose ".github/linters/$rule"
 done
+
+if [[ $MISSING_MEGALINTER_CONFIG == "true" ]]; then
+  rm --force --verbose ".mega-linter.yml"
+fi
