be4ca5dee700919e33516492ff787a7f62980251
liblaf <30631553+liblaf@users.noreply.github.com>
2025-03-23 17:37:28 +0800

    feat(scripts): add Python-based secret management script
    - Introduces `secret-set.py` for managing GitHub Actions secrets programmatically
    - Adds `pynacl` and `typer` dependencies for encryption and CLI functionality
    - Enhances `secret-set.sh` to handle multiple secrets and disable shellcheck warning

--- scripts/secret-set.sh
+++ scripts/secret-set.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC2034
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -12,6 +13,14 @@ readarray -t repos < <(
     --source
 )
 
+CARGO_REGISTRY_TOKEN="$(rbw get --field "TOKEN" "crates.io")"
+GH_APP_ID="$(rbw get --field "APP_ID" "GitHub App")"
+GH_APP_PRIVATE_KEY="$(rbw get --field "notes" "GitHub App")"
+GH_PAT="$(rbw get --field "PAT" "GitHub")"
+NPM_TOKEN="$(rbw get --field "TOKEN" "npm")"
+
 for repo in "${repos[@]}"; do
-  gh secret --repo "$repo" set PAT --body "$PAT"
+  for secret in CARGO_REGISTRY_TOKEN GH_APP_ID GH_APP_PRIVATE_KEY GH_PAT NPM_TOKEN; do
+    gh secret --repo "$repo" set "$secret" --body "${!secret}"
+  done
 done
