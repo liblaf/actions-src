35fbb631782268deed6110d378b7a5aeb75c58ed
liblaf <30631553+liblaf@users.noreply.github.com>
2025-11-24 21:01:43 +0800

    fix(mega-linter): Handle .ruff.toml when checking for missing linter files
    The `pre.sh` script for the `mega-linter` action was not correctly handling `.ruff.toml` files when checking for missing linter configuration files. Previously, it would copy `.ruff.toml` even if a `pyproject.toml` file existed, leading to redundant files.
    
    This change modifies the script to check for the existence of `pyproject.toml` before copying `.ruff.toml`, ensuring that `.ruff.toml` is only copied if neither `.ruff.toml` nor `pyproject.toml` already exist in the working directory. This prevents unnecessary file copying and potential conflicts.

--- actions/mega-linter/pre.sh
+++ actions/mega-linter/pre.sh
@@ -8,7 +8,10 @@ LINTER_RULES_PATH='tmp.6NE8UxMvCk/linters'
 MISSING_FILES=()
 while IFS= read -d '' -r file; do
   filename="$(basename -- "$file")"
-  if [[ -f $filename ]]; then continue; fi
+  case "$filename" in
+    .ruff.toml) if [[ -f $filename ]] || [[ -f "pyproject.toml" ]]; then continue; fi ;;
+    *) if [[ -f $filename ]]; then continue; fi ;;
+  esac
   cp --archive --no-target-directory --verbose "$file" "$filename"
   MISSING_FILES+=("$filename")
 done < <(find "$LINTER_RULES_PATH" -type f -print0)
